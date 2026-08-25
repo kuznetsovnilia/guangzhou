86a43139613f5e1e8da95d3f317bf0a3bd904308
kuznetsovnilia <kuznetsov.n.ilia@gmail.com>
2026-08-25 15:53:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gz-guide-7ee78995bb';
+const CACHE = 'gz-guide-c748c2e17b';
 const TILES = 'gz-tiles';
 const SHELL = ['./', './index.html', './manifest.webmanifest',
   './icon-180.png', './icon-192.png', './icon-512.png'];
